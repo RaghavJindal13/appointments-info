8858cc9832b2fb38f2bb3559b9ebc1d25fe3ac63
Raghav Jindal <raghavjindal131@gmail.com>
2020-08-17 00:27:27 +0530

    Update MainSection.js

--- src/Components/MainSection.js
+++ src/Components/MainSection.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react'
 import ReactPencil from 'react-pencil'
 
 export default class MainSection extends Component {
+    constructor(props) {
+        super(props)
+    }
+
     render() {
-        const { info } = this.props
-        const adresses = info[0].Adress;
+        const { infor, change, onchange } = this.props
+        const { display, value, UserInfo, style } = infor
+        const adresses = UserInfo[0].Adress;
         var adress = adresses.map((adress, index) => {
             if (index === 0) {
                 return (
@@ -22,29 +27,34 @@ export default class MainSection extends Component {
         })
         return (
             <div className="mainsection">
-                <h1>Personal Information</h1>
-                <ReactPencil value={info[0].FirstName}/>
+                <h1>Personal UserInformation</h1>
+                <div style={style} >
+
+                    <input name="value" onChange={onchange} />
+                    <button onClick={change}>Change</button>
+
+                </div>
                 <div>
                     <table>
                         <tbody>
                             <tr>
-                                <td> <strong>First Name: </strong>{info[0].FirstName}</td>
-                                <td className="icon"><i className='fas fa-pencil-alt'></i></td>
+                                <td> <strong>First Name </strong>{UserInfo[0].FirstName}</td>
+                                <td className="icon"><button onClick={this.props.clicked} value="FirstName"><i className='fas fa-pencil-alt' value="FirstName"></i></button></td>
                             </tr>
                             <tr>
-                                <td> <strong>Last Name: </strong>{info[0].LastName}</td>
-                                <td className="icon"><i className='fas fa-pencil-alt'></i></td>
+                                <td> <strong>Last Name </strong>{UserInfo[0].LastName}</td>
+                                <td className="icon"><button onClick={this.props.clicked} value="LastName"><i className='fas fa-pencil-alt' value="LastName"></i></button></td>
                             </tr>
                             <tr>
-                                <td> <strong>User Name: </strong>{info[0].UserName}</td>
+                                <td> <strong>User Name </strong>{UserInfo[0].UserName}</td>
                             </tr>
                             <tr>
-                                <td> <strong>Phone No: </strong>{info[0].PhoneNo}</td>
-                                <td className="icon"><i className='fas fa-pencil-alt'></i></td>
+                                <td> <strong>Phone No </strong>{UserInfo[0].PhoneNo}</td>
+                                <td className="icon"><button onClick={this.props.clicked} value="PhoneNo"><i className='fas fa-pencil-alt' value="PhoneNo"></i></button></td>
                             </tr>
                             <tr>
-                                <td> <strong>Email ID: </strong> {info[0].Email}</td>
-                                <td className="icon"><i className='fas fa-pencil-alt'></i></td>
+                                <td> <strong>Email ID </strong> {UserInfo[0].Email}</td>
+                                <td className="icon"><button onClick={this.props.clicked} value="Email"><i className='fas fa-pencil-alt' value="Email"></i></button></td>
                             </tr>
                             {adress}
                         </tbody>
